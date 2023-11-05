a50c4df0fc4e84c182cdab65e42b0dc3e15168f2
Ryan <ryan.stewart113@gmail.com>
2023-11-04 20:09:19 -0400

    update streamlit container build. Implement basic UI functionality for uploading content from url

--- streamlit_app/src/pages/3_RAG_App.py
+++ streamlit_app/src/pages/3_RAG_App.py
@@ -1,15 +1,62 @@
-import streamlit as st
+from io import BytesIO
+from glob import glob
+import os
+import json
 
-def upload_to_filesystem():
-    raise NotImplementedError()
-
 import streamlit as st
+from langchain.document_loaders import UnstructuredURLLoader
 from torch.cuda import is_available as cuda_is_available
 
 from llm_utils import MODEL_NAMES, AppModel
-from prompt_templates import CHAT_PROMPT_TEMPLATE
+
+SOURCE_DOCS_DIR = "./uploaded_files/source/"
+PROCESSED_DOCS_DIR = "./uploaded_files/processed/"
+
+os.makedirs(SOURCE_DOCS_DIR, exist_ok=True)
+os.makedirs(PROCESSED_DOCS_DIR, exist_ok=True)
+
+error = st.empty()
+
+def write_to_fs(file_content: BytesIO | str, filename: str, upload_dir=SOURCE_DOCS_DIR):
+    
+    if type(file_content) == BytesIO:
+        mode = "wb+"
+    elif type(file_content) == str:
+        mode = "w+"
+    else:
+        raise TypeError("file_content must be BytesIO or str")
+    with open(f"{upload_dir}{filename}", mode) as f:
+        f.write(file_content)
+    
+if not "sources" in st.session_state:
+    st.session_state["sources"] = []
+
+def fetch_available_sources():
+    st.session_state["sources"] = glob(f"{SOURCE_DOCS_DIR}*")
+    return st.session_state["sources"]
+
+def index_chroma_docs(docs, collection_name:str):
+    ...
+def store_and_index_pdf():
+    ...
+
+def store_and_index_html(url:str):
+    loader = UnstructuredURLLoader(urls=[url])
+    
+    try:
+        html_doc = loader.load()[0]
+    except IndexError as e:
+        error.error("URL Likely Invalid")
+        raise e
+    write_to_fs(json.dumps(html_doc.dict()), f"{url.split('/')[-1]}.json")
+    
 
 with st.sidebar:
+
+    available_sources = fetch_available_sources()
+    selected_document = st.selectbox("Source Document", 
+                                     options=available_sources,
+                                     key="selected_document")    
     
     with st.form("Model Settings"):
         st.header("Model Settings")
@@ -24,14 +71,36 @@ with st.sidebar:
             with st.spinner("Loading model"):
                 st.session_state.model = AppModel(model_name=model_name)
             st.write(f"Model {model_name} loaded successfully")
+    
+    st.divider()
 
-if not "model" in st.session_state:
-    st.header("*Load a model to get started*")
+    st.header("Add Content")
+    
+    # Handle File Uploads
+    # uploaded_file = st.file_uploader("Upload a File *(PDF Only)*")
+    # if uploaded_file is not None:
+    #     filename = uploaded_file.name
+    #     # To read file as bytes:
+    #     bytes_data = BytesIO(uploaded_file.getvalue())
+
+    #     write_to_fs(file_content=bytes_data, filename=filename)
+        
+        
+
+    entered_url = st.text_input("Enter a URL", key="entered_url")
+    if entered_url:
+        store_and_index_html(entered_url)
+        del st.session_state["entered_url"]
+        st.rerun()
+
+if not "model" in st.session_state or not "selected_document" in st.session_state:
+    st.header("*Select a source document and load a model to get started*")
 else:
     model = st.session_state.model
 
     st.title("Retrieval Augmented Generation")
     st.caption(f"Using model {model._model_name}")
+    st.caption(f"Using document {st.session_state.selected_document}")
     st.divider()
 
     
